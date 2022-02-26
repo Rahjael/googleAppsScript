98fa301fe66558988d0d0ca0bd6e739a809d7aa2
Rahjael <surfer.vortex@gmail.com>
2022-02-26 01:43:23 +0100

    code refactored

--- appFuelPrices/functions.js
+++ appFuelPrices/functions.js
@@ -5,6 +5,11 @@ function performDailyRoutine() {
   Logger.log('Daily routine correctly performed.');
 }
 
+function downloadOpenDataCSVsToDrive() {
+  downloadPricesCSV();
+  downloadStationsCSV();
+}
+
 function generateUID() {
   // Helper function to generate a UNIQUEID the same way appsheet does
   let values = '0123456789abcdefghijklmnopqrstuvwxyz';
@@ -45,9 +50,11 @@ function parseLatLong(stringData) {
 
   if(typeof stringData != 'string') throw Error('data is not a string');
 
-  // Get how many digits the integer part has
-  const indexOfSeparator = stringData.indexOf('.');
+  // Sanitize spaces before proceeding. Many records have spaces in front...
+  stringData = stringData.replace(' ', '');
 
+  // Get how many digits the integer part has and return the parsed value
+  const indexOfSeparator = stringData.indexOf('.');
   if(indexOfSeparator === 1) {
     return stringData.slice(0, 7);
   }
@@ -66,13 +73,14 @@ function updateStationsDatabase() {
   const stringData = DriveApp.getFolderById(CONFIG.APP_FOLDER_ID).getFilesByName(CONFIG.STATIONS_CSV_FILENAME).next().getBlob().getDataAsString();
   const data = parseCSVStringDataTo2dArray(stringData); // 2d array
 
-
+  /*
   // Replace dot with comma in latitude and longitude to prevent SpreadsheetApp problems when pasting value
+  // There is no need to do this with proper settings in the spreadsheet locale
   data.forEach( (row,i) => {
-    if(i === 0) return;
     row[8] = parseLatLong(row[8]).replace('.', ','); // latitude
     row[9] = parseLatLong(row[9]).replace('.', ','); // longitude
   });
+  */
 
   // Paste data to the proper datasheet, after clearing it
   const stationsSheet = SpreadsheetApp.openById(CONFIG.SHEET_DATABASE_ID).getSheetByName(CONFIG.STATIONS_SHEET_NAME);
@@ -98,18 +106,21 @@ function updatePricesDatabase() {
     }
   });
 
+  /*
   // Replace dot with comma in prices to prevent SpreadsheetApp problems when pasting value
+  // There is no need to do this with proper settings in the spreadsheet locale
   data.forEach( (row, i) => {
     if(i === 0) return;
     row[3] = row[3].replace('.', ','); // prices
   });
+  */
 
-/*
+  /*
   for(let i = 0; i < 100; i++) {
     Logger.log(data[i]);
   }
   return;
-*/
+  */
   // Paste data to the proper datasheet, after clearing it
   const pricesSheet = SpreadsheetApp.openById(CONFIG.SHEET_DATABASE_ID).getSheetByName(CONFIG.PRICES_SHEET_NAME);
   pricesSheet.clearContents();
@@ -118,33 +129,56 @@ function updatePricesDatabase() {
   Logger.log(`Contents of file ${CONFIG.PRICES_CSV_FILENAME} have been copied to the database. (${Date.now() - startTime} ms)`);
 }
 
-function downloadOpenDataCSVsToDrive() {
-  // Cleanup already existing versions...
-  // TODO this part assumes the following download will be alright.
-  // The potential risk is to delete the files without being able to get new ones. FIX THIS
-  [CONFIG.PRICES_CSV_FILENAME, CONFIG.STATIONS_CSV_FILENAME].forEach( filename => {
-    const iterator = DriveApp.getFolderById(CONFIG.APP_FOLDER_ID).getFilesByName(filename);
+
+function downloadPricesCSV() {
+  const url = CONFIG.PRICES_URL;
+  const filename = CONFIG.PRICES_CSV_FILENAME;
+  const foldername = CONFIG.APP_FOLDER_ID;
+  // This returns an HTTPResponse object
+  const response = UrlFetchApp.fetch(url);
+
+  // If response is good
+  if(response.getResponseCode() === 200) {
+    // Delete old file
+    const iterator = DriveApp.getFolderById(foldername).getFilesByName(filename);
     while(iterator.hasNext()) {
       let file = iterator.next();
       file.setTrashed(true);
     }
-  });
-  Logger.log(`Old OpenData files deleted`);
-
-  // ... then download new versions
-  [CONFIG.PRICES_URL, CONFIG.STATIONS_URL].forEach( url => {
-    // This returns an HTTPResponse object
-    const response = UrlFetchApp.fetch(url);
-
-    // If response is good, make it a blob and create a file
-    if(response.getResponseCode() === 200) {
-      const fileBlob = response.getBlob();
-      const appFolder = DriveApp.getFolderById(CONFIG.APP_FOLDER_ID);
-      appFolder.createFile(fileBlob);
-      Logger.log(`${fileBlob.getName()} of type ${fileBlob.getContentType()} created in app folder.`);
-    }
-    else {
-      Logger.log(`Something went wrong while fetching resource at ${url}`);
-    }
-  });
+    // Create new file
+    const fileBlob = response.getBlob();
+    const appFolder = DriveApp.getFolderById(foldername);
+    appFolder.createFile(fileBlob);
+    Logger.log(`${fileBlob.getName()} of type ${fileBlob.getContentType()} created in app folder.`);
+  }
+  else {
+    Logger.log(`Something went wrong while fetching resource at ${url}. Old file has not been deleted.`);
+  }
 }
+
+function downloadStationsCSV() {
+  const url = CONFIG.STATIONS_URL;
+  const filename = CONFIG.STATIONS_CSV_FILENAME;
+  const foldername = CONFIG.APP_FOLDER_ID;
+  // This returns an HTTPResponse object
+  const response = UrlFetchApp.fetch(url);
+
+  // If response is good
+  if(response.getResponseCode() === 200) {
+    // Delete old file
+    const iterator = DriveApp.getFolderById(foldername).getFilesByName(filename);
+    while(iterator.hasNext()) {
+      let file = iterator.next();
+      file.setTrashed(true);
+    }
+    // Create new file
+    const fileBlob = response.getBlob();
+    const appFolder = DriveApp.getFolderById(foldername);
+    appFolder.createFile(fileBlob);
+    Logger.log(`${fileBlob.getName()} of type ${fileBlob.getContentType()} created in app folder.`);
+  }
+  else {
+    Logger.log(`Something went wrong while fetching resource at ${url}. Old file has not been deleted.`);
+  }
+}
+
