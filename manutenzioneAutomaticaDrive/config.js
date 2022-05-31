bfe753a08cc5fed72efdf5a3153fd140a68f54d9
Rahjael <surfer.vortex@gmail.com>
2022-05-31 11:18:02 +0200

    implemented scanner folder cleanup

--- manutenzioneAutomaticaDrive/config.js
+++ manutenzioneAutomaticaDrive/config.js
@@ -1,3 +1,9 @@
 /***
  *    this file is empty for privacy reasons
  */
+
+ const CONFIG = {
+  MAIN_NOTIFICATION_ADDRESS: '',
+  NOTIFICATION_ADDRESSES: ['', '', ''],
+  EMAIL_DEFAULT_SUBJECT: 'Notifica da pulizia automatica Drive'
+}
--- manutenzioneAutomaticaDrive/functions.js
+++ manutenzioneAutomaticaDrive/functions.js
@@ -8,6 +8,68 @@
   Logger.log(`Email sent to ${addresses}`);
 }
 
+
+function dailyCleanup() {
+  scannerFolderCleanup();
+  rootFolderCleanup();
+}
+
+
+// THIS FUNCTION IS READY BUT ACTIVATOR IS NOT SET
+function scannerFolderCleanup() {
+  Logger.log('Initiating root folder cleanup...');
+
+  // This will be reused later, no const needed
+  let notificationBody = '\n\nI seguenti file erano presenti nella cartella "Scansioni su computer Palma" e sono stati eliminati: \n\n';
+  let deletedFiles = [];
+
+  // Get all files in root folder, set them as trashed and log filename
+
+  const folderIterator = DriveApp.getFoldersByName('Scansioni su computer Palma');
+
+  const folders = (() => {
+    // return all found folders. throw if more than 1 folder is found
+    let folders = [];
+    while(folderIterator.hasNext()) {
+      folders.push(folderIterator.next());
+    }
+    if(folders.length != 1) {
+      throw Error(`Found ${folders.length} folders with that name and that shouldn't happen`);
+    }
+    return folders;
+  })();
+
+  // Iterate over files in that folder
+  const iterator = folders[0].getFiles();
+  let file;
+  while(iterator.hasNext()) {
+    file = iterator.next();
+    deletedFiles.push(file.getName());
+    file.setTrashed(true);
+    //Logger.log(`FAKE: setTrashed ${file.getName()}`);
+  }
+
+  // Send email notification about deletion, if necessary
+  if(deletedFiles.length === 0) {
+    Logger.log('No cleanup to be done.');
+    return;
+  }
+  else {
+    deletedFiles = '- ' + deletedFiles.join('\n- ');
+    notificationBody += deletedFiles;
+    sendEmailNotification(CONFIG.NOTIFICATION_ADDRESSES, notificationBody);
+    //Logger.log(`FAKE: sent email ${notificationBody}`);
+    Logger.log('Cleanup done.');
+  }
+}
+
+
+
+
+
+
+
+
 function rootFolderCleanup() {
   Logger.log('Initiating root folder cleanup...');
 
@@ -30,11 +92,11 @@ function rootFolderCleanup() {
     return;
   }
   else {
-    deletedFiles = deletedFiles.join('\n - ');
+    deletedFiles = '- ' + deletedFiles.join('\n - ');
     notificationBody += deletedFiles;
     sendEmailNotification(CONFIG.MAIN_NOTIFICATION_ADDRESS, notificationBody);
     Logger.log('Cleanup done.');
-  }  
+  }
 }
 
 
